e1647b4bc4867440b29e31efc2e607266885b571
Mahmoud AlSofyani <msafar95@hotmail.com>
2020-11-09 12:37:17 +0400

    kind of finished making the main dashboard ui layout

---
+++ app/src/main/java/com/example/csit242_project/Activities/MainDashboardActivity.java
@@ -0,0 +1,21 @@
+package com.example.csit242_project.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.csit242_project.R;
+
+public class MainDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maindashboard_activity);
+    }
+
+
+}
